800eeb10db3b94299e5506c243479531e5c102b3
Awen-hub <920733056@qq.com>
2022-01-07 21:02:14 +0800

    refactor(git): 重构自定义提交相关代码

--- push.mjs
+++ push.mjs
@@ -16,6 +16,68 @@ const { version: lastVersion } = require("./package");
 
 const { log } = console;
 
+const handleErr = (err, message) => {
+  if (message !== "") {
+    log(chalk.red(message));
+  }
+  log(err);
+  process.exit(1);
+};
+
+const handleResolve = (stdout, message, resolve) => {
+  log(chalk.blue(message));
+  log(stdout);
+  resolve();
+};
+
+const gitAdd = () =>
+  new Promise((resolve) => {
+    exec("git add .", (err, stdout) => {
+      if (err) {
+        handleErr(err, "git add . 执行出错");
+      }
+      handleResolve(stdout, "git add . 执行成功", resolve);
+    });
+  });
+
+const gitCommit = () =>
+  new Promise((resolve) => {
+    exec(
+      `git commit -m "misc: 修改package.json和AppLog.json" -n`,
+      (err, stdout) => {
+        if (err) {
+          handleErr(err, "git commit执行出错");
+        }
+        handleResolve(stdout, "git add . 执行成功", resolve);
+      }
+    );
+  });
+
+const getBranch = () =>
+  new Promise((resolve) => {
+    exec("git rev-parse --abbrev-ref HEAD", (err, stdout) => {
+      if (err) {
+        handleErr(err, "获取当前分支名字时发生错误");
+      }
+      log(chalk.blue("成功获取当前分支名称"));
+      resolve(stdout.replace("\n", ""));
+    });
+  });
+
+const gitPush = (nowBranch) =>
+  new Promise((resolve) => {
+    exec(
+      `git push --set-upstream origin ${nowBranch} --no-verify --tags`,
+      (err, stdout) => {
+        if (err) {
+          handleErr(err, "git push发生错误");
+        } else {
+          handleResolve(stdout, "git push行成功", resolve);
+        }
+      }
+    );
+  });
+
 let promptList = [
   {
     type: "list",
@@ -27,6 +89,7 @@ let promptList = [
 ];
 
 const { isChangeVersion } = await inquirer.prompt(promptList);
+
 if (isChangeVersion === "Yes") {
   promptList = [
     {
@@ -35,15 +98,15 @@ if (isChangeVersion === "Yes") {
       name: "versionNumber",
       validate(version) {
         if (!compareVersions.validate(version)) {
-          log(chalk.red("\n版本号不规范, 请重新填写"));
+          log(chalk.red("\n版本号不规范, 请重新填写\n"));
           return false;
         }
         if (version.split(".").length !== 3) {
-          log(chalk.red("\n版本号格式必须为x.y.z形式, 请重新填写"));
+          log(chalk.red("\n版本号格式必须为x.y.z形式, 请重新填写\n"));
           return false;
         }
         if (compareVersions.satisfies(version, lastVersion)) {
-          log(chalk.red("\n当前版本号必须大于上一个版本号, 请重新填写"));
+          log(chalk.red("\n当前版本号必须大于上一个版本号, 请重新填写\n"));
           return false;
         }
         return true;
@@ -54,7 +117,7 @@ if (isChangeVersion === "Yes") {
   promptList = [
     {
       type: "input",
-      message: `请填写更新日志(按一次回车键换行, 按两次回车键结束填写)：`,
+      message: `请填写更新日志(按一次回车键换行, 连续按两次回车键结束填写)：`,
       name: "logContent",
     },
   ];
@@ -80,54 +143,21 @@ if (isChangeVersion === "Yes") {
     flag: "w+",
   });
 
-  const updateAppLogJSON = require("./updateAppLog");
+  const AppLog = require("./AppLog");
   const time = dayjs.tz(dayjs(), "Asia/Shanghai").format("YYYY-MM-DD");
-  updateAppLogJSON.data.push({
+  AppLog.data.push({
     content: logList,
     title: `${time} 新版本(V${versionNumber})`,
   });
-  await writeFile(
-    "updateAppLog.json",
-    JSON.stringify(updateAppLogJSON, null, 2),
-    { flag: "w+" }
-  );
+  await writeFile("AppLog.json", JSON.stringify(AppLog, null, 2), {
+    flag: "w+",
+  });
 
-  await new Promise((resolve) => {
-    exec("git add .", (err, stdout) => {
-      log(stdout)
-      resolve();
-    });
-  });
-  
-  await new Promise((resolve) => {
-    exec(
-      `git commit -m "misc: 修改package.json和updateAppLog.json" -n`,
-      (err, stdout) => {
-        if (err) {
-          log(chalk.red("commit出错"))
-        }
-        log(stdout)
-        resolve();
-      }
-    );
-  });
+  await gitAdd();
+
+  await gitCommit();
 }
 
-const nowBranch = await new Promise((resolve) => {
-  exec("git rev-parse --abbrev-ref HEAD", (err, stdout) => {
-    resolve(stdout.replace("\n", ""));
-  });
-});
+const nowBranch = await getBranch();
 
-await new Promise((resolve) => {
-  exec(`git push --set-upstream origin ${nowBranch} --no-verify --tags`, (err, stdout) => {
-    if (err) {
-      log(chalk.red("push出错"));
-      log(err);
-    } else {
-      log(chalk.blue("push成功"));
-      log(stdout);
-    }
-    resolve();
-  });
-});
+await gitPush(nowBranch);
